a4f86e457fb4a3c18cabbb7caddca46bbf03ed23
Robin Andersson <robin.andersson@stretch.se>
2018-04-13 17:07:13 +0200

    Stage all

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Drillytical
 Web client for training app, input data for each exercise. Overview of your training progress.
+
+## commands
+### npm run dev
+### npm run build
---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="login">
+    <b-container>
+        <div class="centerblock">
+            <h1>Drillytical - login page</h1>
+            <p>Your training logging app</p>
+            <b-form id="loginform">
+                <b-input placeholder="Username" />
+                <b-input placeholder="Password" />
+                <b-button variant="primary">Login</b-button>
+                <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+            </b-form>
+        </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components: {
+    
+  },
+  created() {
+      
+  }
+}
+</script>
+<style lang="scss">
+body {
+    background: linear-gradient(to bottom right, rgba(73, 73, 73, 0.678)), url('../assets/trainingbg.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+
+.centerblock {
+    margin: 20em auto;
+    max-width: 400px;
+}
+
+#login {
+    color: rgba(255, 255, 255, 0.8);
+}
+
+#loginform {
+    font-family: 'Abel', sans-serif;
+    margin-top: 0.5em;
+    width: 100%;
+    
+
+    input {
+        max-width: 400px;
+        margin-bottom: 0.5em;
+    }
+
+    button {
+        margin-right: 0.5em;
+    }
+}
+</style>
